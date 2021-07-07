5867ce131659f8fb6091385a10277e04bd0fe5e5
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2021-07-07 15:10:29 +0600

    made variable and type name different

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as p5 from 'p5';
 
-const Sketch = (p5: p5) => {
-  p5.setup = () => {
-    p5.createCanvas(400, 400);
+const Sketch = (sketch: p5) => {
+  sketch.setup = () => {
+    sketch.createCanvas(400, 400);
   };
 
-  p5.draw = () => {
-    p5.background(51);
+  sketch.draw = () => {
+    sketch.background(51);
   };
 };
 
